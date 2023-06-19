91c2a6fd507ccfbe2b319f9d266663018e288f9b
kaliostroeva <126382109+kaliostroeva@users.noreply.github.com>
2023-06-19 11:20:10 +0300

    Add files via upload

---
+++ tree.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+struct TrieNode {
+    vector<TrieNode*> children;
+    bool isEndOfWord;
+    TrieNode() : children(26), isEndOfWord(false) {}
+};
+
+void insert(TrieNode* root, string word) {
+    TrieNode* node = root;
+    for (char ch : word) {
+        int index = ch - 'a';
+        if (!node->children[index])
+            node->children[index] = new TrieNode();
+        node = node->children[index];
+    }
+    node->isEndOfWord = true;
+}
+
+void search(TrieNode* node, string word, vector<string>& suggestions) {
+    if (node->isEndOfWord)
+        suggestions.push_back(word);
+    for (int i = 0; i < 26; i++) {
+        if (node->children[i])
+            search(node->children[i], word + char(i + 'a'), suggestions);
+    }
+}
+
+vector<string> autocomplete(TrieNode* root, string prefix) {
+    vector<string> suggestions;
+    TrieNode* node = root;
+    for (char ch : prefix) {
+        int index = ch - 'a';
+        if (!node->children[index])
+            return suggestions;
+        node = node->children[index];
+    }
+    search(node, prefix, suggestions);
+    return suggestions;
+}
+
+int main() {
+    vector<string> dictionary = { "apple", "banana", "cherry", "grape", "lemon", "orange", "peach", "pear" };
+    TrieNode* root = new TrieNode();
+    for (string word : dictionary)
+        insert(root, word);
+
+    string input;
+    while (true) {
+        cout << "Enter a prefix: ";
+        cin >> input;
+        vector<string> suggestions = autocomplete(root, input);
+        cout << "Suggestions: ";
+        for (string suggestion : suggestions)
+            cout << suggestion << " ";
+        cout << endl;
+    }
+
+    return 0;
+}
